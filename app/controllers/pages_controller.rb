c3a868f0b53a2bbe03c9d8c89ad3c33bf3cecd0c
arun2797 <arunachalam2797@gmail.com>
2018-12-19 16:56:09 +0530

    5. Created a Post Model to add new tweets in CommandLine. View Tweets in Home,Profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,26 @@ class PagesController < ApplicationController
   end
 
   def home
+
+  	@posts = Post.all
+
   end
 
   def explore
+
+  	@posts = Post.all
+
   end
 
   def profile
+
+  	if ( User.find_by_email(params[:id]) )
+  		@username = params[:id]
+  	else
+  		redirect_to root_path, :notice => "User Not Found"
+  	end
+
+  	@posts = Post.all.where("user_id=?",User.find_by_email(params[:id]).id)
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'pages#index'
   get '/home' => 'pages#home'
   get '/explore' => 'pages#explore'
-  get '/profile' => 'pages#profile'
+  get '/user/:id' => 'pages#profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
